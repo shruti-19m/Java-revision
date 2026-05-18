82120c1d1188b2741da8eddbddb47fe482505e9d
shruti-19m <shrutimandal97293512@gmail.com>
2026-05-18 11:21:06 +0530

    changes added

--- First.java
+++ First.java
@@ -2,16 +2,60 @@ public class First {
 
    public static void main(String[] args) { 
       //write ur code here
-      System.out.println("Velora");
+      //System.out.println("Velora");
 
-      /*variables
-      String name ="Aman";
-      int age = 30;
-
-      String neighbour = "Akku";
+      /*
+      ____PRIMITIVE TYPES______
+      
+      variables
+      String name ="velora";
+      int age = 21;
+      String neighbour = "Amoremio";
 
       String friend = neighbour;
-      */
+      
+
+       TYPES
+      1.byte - 1 ( -128 to 127)
+      2.short - 2
+      3.int - 4 ( 1,2,3,....)
+      4.long- 8
+      5. float - 4 ( 3.14)
+      6. Double -8
+      7. char -2 (a,b,c,d)
+      8.boolean -1 (true/false)
+       
+
+      //____EXAMPLE___
+     
+       byte age = 12;
+       int phone = 1234567890;
+       long phone2 =12345678900L;
+       float pi = 3.14F;
+       char letter = '@';
+       boolean isAdult = false;
+       */
+
+       /*
+       NON-PRIMITIVE TYPES
+
+       String name = "velora";
+       String friend = new String( "vincci");
+       System.out.println(name.length());
+       */
+       
+       //Strings
+       //concatenate-----:adding two string.
+
+       String name1 ="velora";
+       String name2 ="amoremio";
+       String name3 =name1 + " and " + name2;
+       System.out.println(name3);
+
+       //CharAt
+        String name ="velora";
+        System.out.println(name.charAt(0));
+
 
 
    }
