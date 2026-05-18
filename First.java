de37bbe430b37b0014d4ed79f49f3c97d6a580a5
shruti-19m <shrutimandal97293512@gmail.com>
2026-05-18 10:52:25 +0530

    commit and push

---
+++ First.java
@@ -0,0 +1,18 @@
+public class First {
+
+   public static void main(String[] args) { 
+      //write ur code here
+      System.out.println("Velora");
+
+      /*variables
+      String name ="Aman";
+      int age = 30;
+
+      String neighbour = "Akku";
+
+      String friend = neighbour;
+      */
+
+
+   }
+}
